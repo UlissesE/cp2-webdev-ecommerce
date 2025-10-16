ec8002654a093b4b3d321c53d8f85ea7f1fb2455
Eduardo <eduardoulisses2006@gmail.com>
2025-10-16 20:05:44 -0300

    feat(card) add card model

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,15 @@
-export default function ProductCard({...props}) {
+export default function ProductCard({titulo, preco, descricao, categoria, imagem, estrelas, avaliacoes}) {
     return (
-        <>
-            
-        </>
+        <div>
+            <img src={imagem} alt="" />
+            <div>
+                <h3>{titulo}</h3>
+                <p>{descricao}</p>
+                <p>{preco}</p>
+                <div>
+                    ⭐ <span>{estrelas} em {avaliacoes} avaliações</span>
+                </div>
+            </div>
+        </div>
     )
 }
