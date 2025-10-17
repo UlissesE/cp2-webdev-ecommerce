e4b678e103f5be4bcf8fc8b6ca1dcb9321fda793
Eduardo <eduardoulisses2006@gmail.com>
2025-10-16 21:09:45 -0300

    feito

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,6 @@
 export default function ProductCard({title, price, image, rating}) {
     return (
-        <div className="border-1 border-amber-950 flex flex-col items-center gap-5 p-5">
+        <div className="border-1 border-black/50 flex flex-col items-center gap-5 p-5">
             <img src={image} alt="" className="w-30 h-auto"/>
             <div className="flex flex-col gap-3">
                 <h3 className="font-bold">{title}</h3>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,7 @@ export default function Section({titulo, children}) {
     return (
         <section className="flex flex-col">
             <h1 className="text-3xl font-bold text-center p-3 bg-amber-200">{titulo}</h1>
-            <div className="flex py-10 gap-10">
+            <div className="flex py-10 px-4 gap-10">
             {children}
             </div>
         </section>
